98a9795bc941a0d79ccb0fafd97e1eac5f1661c9
lihouhua <lihouhua@corp.netease.com>
2022-08-09 10:56:57 +0800

    反射demo

---
+++ sb-common-demo/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * ${description}
+ *
+ * @author: LengShui on ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ **/
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
